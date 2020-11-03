cc21149f2cd83e58b04b7fa2d448c3b1df30ab47
rtdeedqgryet <borismariondorier@gmail.com>
2020-11-03 19:09:17 +0100

    mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@obscure-mesa-53509.herokuapp.com'
+ 
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'obscure-mesa-53509.herokuapp.com/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+  end
+end
